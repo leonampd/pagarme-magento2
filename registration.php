fe6b9a0952bb8725143de935d951eacba9fd1d13
Leonam Pereira Dias <leonam.dias@pagar.me>
2018-01-23 10:25:06 -0200

    Move module files to root dir
    Looking at some modules repos, I see that all files are in root dir.

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PagarMe_CreditCard',
+    'PagarMe_Magneto',
     __DIR__
 );
