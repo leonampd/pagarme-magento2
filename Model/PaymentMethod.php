7bce176167122e236e0e2fb6228e600d8c205cb4
Leonam Pereira Dias <leonam.dias@pagar.me>
2018-01-24 12:18:14 -0200

    Remove files

--- Model/PaymentMethod.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Test\Testpayment\Model;
-
-class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
-{
-    protected $_code = 'pagarme_creditcard';
-}
--- registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PagarMe_Magneto',
-    __DIR__
-);
